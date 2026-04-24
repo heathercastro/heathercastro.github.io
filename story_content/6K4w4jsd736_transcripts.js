5c5be2e5a1855da2b2c4fd467f724b0e896477b8
heathercastro <hrc7@txstate.edu>
2026-04-24 15:53:38 -0500

    latest

---
+++ story_content/6K4w4jsd736_transcripts.js
@@ -0,0 +1,5 @@
+
+(function() {
+    const data = {"transcripts":[{"name":"captions","cues":[{"start":209,"text":"Thank you for participating in this course,"},{"start":2180,"text":"Reading Made Simple."},{"start":3400,"text":"I hope it has sparked your curiosity to learn more about the research and ways you can"},{"start":7251,"text":"implement daily routines that will make a big impact"},{"start":9870,"text":"on your students' ability to read and write."}]}]};
+    window.globalLoadJsAsset('story_content/6K4w4jsd736_transcripts.js', JSON.stringify(data));
+})();
